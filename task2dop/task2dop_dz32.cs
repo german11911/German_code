cd8b82a9d8127f21aab6d4bb8927ee7effe87bf0
German <german@serpantin.ru>
2023-05-29 19:35:04 +0300

    29_05_23_3

---
+++ task2dop/task2dop_dz32.cs
@@ -0,0 +1,13 @@
+﻿// ввести целое N
+System.Console.Write("Введите положительное целое число - > ");
+int n = int.Parse(System.Console.ReadLine()!);
+int m = n%10;
+int mm = (n/10);
+mm =mm%10;
+Console.Write(n);
+Console.Write(" ");
+if(mm==1 || m ==0 || m>=5){
+    Console.WriteLine("программистов");
+   }
+else {if(m==1)Console.WriteLine("программист");
+    else Console.WriteLine("программиста");}
