167a4558a9fe73288a7c106dee89e88d289fda48
German <german@serpantin.ru>
2023-05-29 16:42:13 +0300

    29_5_23

---
+++ task10/task10_dz32.cs
@@ -0,0 +1,10 @@
+﻿// ввести целое число, вывести 2ю цифру слева 
+System.Console.Write("Введите целое число - > ");
+int n = int.Parse(System.Console.ReadLine()!);
+if(n>=10){
+    while(n>99)
+        n/=10;
+    int m =n%10; 
+    System.Console.WriteLine(m);
+}
+else System.Console.WriteLine("нет второй цифры слева");
---
+++ task13/task13_dz32.cs
@@ -0,0 +1,11 @@
+﻿// ввести 3ю цифру слева целого
+System.Console.Write("Введите целое число - > ");
+int n = int.Parse(System.Console.ReadLine()!);
+if(n>=100){
+    while(n>999)
+        n/=10;
+    int m =n-(n/100)*100; 
+    m =m%10;
+    System.Console.WriteLine(m);
+}
+else System.Console.WriteLine("нет третьей цифры слева");
