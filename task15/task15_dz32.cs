67f3df502828394d962adb6fdb6d09e655f666f4
German <german@serpantin.ru>
2023-05-29 18:57:22 +0300

    29_05_23_2

---
+++ task15/task15_dz32.cs
@@ -0,0 +1,9 @@
+﻿// ввести цифру 1-7 -день недели. проверить, является ли выходным?
+System.Console.Write("Введите целое число от 1 до 7 - > ");
+int n = int.Parse(System.Console.ReadLine()!);
+if(n==7 || n==6){
+   System.Console.WriteLine("Да, это выходной");}
+else {
+   if(n<=5 && n>=1)
+System.Console.WriteLine("Нет, это рабочий день");
+else System.Console.WriteLine("нет такого дня недели!");}
---
+++ task1dop/task1dop_dz32.cs
@@ -0,0 +1,20 @@
+﻿// удалить вторую цифру слева 3ю цифру слева целого
+System.Console.Write("Введите целое, или вещественное число - > ");
+double n = double.Parse(System.Console.ReadLine()!);
+double x = n;
+if(n>=10){
+    int l = 0;
+    while(x>99){
+        x/=10;
+        l++;}
+    int vtoraya =(int) x%10;
+    int pervaya = (int) (x / 10);
+  //  double rest = x - m -pervaya*10;
+    while (l>0){
+        pervaya*=10;
+        vtoraya*=10;
+        l--;}
+        System.Console.WriteLine(n-vtoraya-9*pervaya);
+        System.Console.WriteLine(9*pervaya);
+        }
+else System.Console.WriteLine("нет второй цифры в целой части");
