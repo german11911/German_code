2cc36430d2dd801a953d3d0c52d73d798b65fb99
German <german@serpantin.ru>
2023-06-05 20:47:40 +0300

    dz34_6

---
+++ DZ34/task29/task29.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintArray(double[] array)
+{
+    foreach (var item in array)
+        Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+double [] array8 = new double [8];
+FillArray(array8);
+PrintArray(array8);
