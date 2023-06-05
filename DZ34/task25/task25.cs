bf67db4bbb5b05b5ff9d524b588fff6ab0c1f2dd
German <german@serpantin.ru>
2023-06-05 18:42:50 +0300

    dz34

---
+++ DZ34/task25/task25.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Double Pow(double a, int b)
+{double result = 1;
+    for (int i=1; i<= b; i++)
+{result *=a;}
+return result;
+}
+System.Console.Write("Введите вещественное число (основание): ");
+double a = double.Parse(Console.ReadLine()!);
+System.Console.Write("Введите натуральное число (степень): ");
+int b = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine($"{a} в степени {b} = {Pow(a,b)}");
---
+++ DZ34/task27/task27.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.Write("Введите вещественное число: ");
+double x = double.Parse(Console.ReadLine()!);
+if(x< 0) x=-x;
+while (x%1>0){
+    x *=10;
+}
+System.Console.WriteLine($"исходное {x}");
