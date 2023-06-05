535797ddb06ef005da0e45c60fe2c17566945d86
German <german@serpantin.ru>
2023-06-05 20:23:13 +0300

    dz34_2

--- DZ34/task27/task27.cs
+++ DZ34/task27/task27.cs
@@ -1,8 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-System.Console.Write("Введите вещественное число: ");
-double x = double.Parse(Console.ReadLine()!);
-if(x< 0) x=-x;
-while (x%1>0){
-    x *=10;
-}
-System.Console.WriteLine($"исходное {x}");
+﻿        Console.Write("Введите вещественное число: ");
+        decimal x = decimal.Parse(Console.ReadLine()!);
+       int ndigits = 0;
+        int sumdigits = 0;
+        if (x < 0) x = -x;
+        if (x==0){
+        ndigits =1;
+        sumdigits = 0;
+        }        
+        else {
+      bool r=false;
+      if(x<1) r=true;
+            int i =0;
+                 while (x % 1 > 0)
+        {
+            x *= 10;
+            i++;
+        }
+                int y = Convert.ToInt32(x);
+        while (y / 10 > 0)
+        {
+            ndigits++;
+            sumdigits += y % 10;
+            y /= 10;
+        }
+         ndigits++;
+            sumdigits += y % 10;
+      if(r) ndigits = i;
+        }
+        Console.WriteLine($"количество цифр {ndigits} сумма цифр {sumdigits}");
+    
